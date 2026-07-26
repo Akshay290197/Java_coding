741ee982fcfbda7d1100f95fa2b35f4b359db36e
Akshay Kumar <cakshay388@gmail.com>
2026-07-26 21:50:03 +0530

    count frequency using map

---
+++ src/main/java/org/example/prac_01/CountFrequency.java
@@ -0,0 +1,17 @@
+package org.example.prac_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i),0)+1);
+        }
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
+}
