38d133e18ace0c7cb225bcb005e756e98b6431fa
Akshay Kumar <akshay_kumar@cms.co.in>
2026-07-27 11:27:36 +0530

    first non repeating character

---
+++ src/main/java/org/example/prac_01/FirstNonRepeatingChar.java
@@ -0,0 +1,26 @@
+package org.example.prac_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+        String str = "programming";
+        Character ch = findFirstNonRepeating(str);
+        System.out.println(ch);
+    }
+
+    public static Character findFirstNonRepeating(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<str.length();i++){
+            if(map.get(str.charAt(i))==1){
+                return str.charAt(i);
+            }
+        }
+        return null;
+    }
+}
