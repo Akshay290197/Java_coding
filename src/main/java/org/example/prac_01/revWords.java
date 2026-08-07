4036eb1ff42f29c7313ccd812c42ca230c42f4e2
Akshay Kumar <akshay_kumar@cms.co.in>
2026-08-07 13:15:34 +0530

    reverse words complete

--- src/main/java/org/example/prac_01/revWords.java
+++ src/main/java/org/example/prac_01/revWords.java
@@ -2,6 +2,14 @@ package org.example.prac_01;
 
 public class revWords {
     public static void main(String[] args) {
-
+        String str = "I love java";
+        System.out.println(reverseWords(str));
+    }
+    public static String reverseWords(String str){
+        String[] word = str.split(" ");
+        for(int i = word.length-1;i>=0;i--){
+            System.out.print(word[i]+" ");
+        }
+        return "";
     }
 }
