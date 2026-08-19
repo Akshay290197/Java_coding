31f0c69ed80d0d11e684ffcaedcc2322a22dd8c5
Akshay Kumar <akshay_kumar@cms.co.in>
2026-08-19 15:45:35 +0530

    two sum

---
+++ src/main/java/org/example/DSA/TwoSum.java
@@ -0,0 +1,18 @@
+package org.example.DSA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                System.out.println(map.get(target-nums[i])+","+i);
+            }
+            map.put(nums[i],i);
+        }
+    }
+}
