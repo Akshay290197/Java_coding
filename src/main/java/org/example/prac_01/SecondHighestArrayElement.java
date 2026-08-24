c2ae0903df4f7398faa37db8315fb1c71ac1753f
Akshay Kumar <akshay_kumar@cms.co.in>
2026-08-24 15:18:29 +0530

    second highest element in array

---
+++ src/main/java/org/example/prac_01/SecondHighestArrayElement.java
@@ -0,0 +1,18 @@
+package org.example.prac_01;
+
+public class SecondHighestArrayElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 20, 15};
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+        for(int num : arr){
+            if(num>highest){
+                secondHighest = highest;
+                highest = num;
+            } else if (num>secondHighest && num!= highest) {
+                secondHighest=num;
+            }
+        }
+        System.out.println(secondHighest);
+    }
+}
