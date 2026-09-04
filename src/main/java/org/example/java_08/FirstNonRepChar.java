4fd6dae7114f49ce4520436e3efe804d9fd47c0e
Akshay Kumar <cakshay388@gmail.com>
2026-09-04 16:35:16 +0530

    first non rep char

---
+++ src/main/java/org/example/java_08/FirstNonRepChar.java
@@ -0,0 +1,24 @@
+package org.example.java_08;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepChar {
+    public static void main(String[] args) {
+        String str = "swiss";
+        Character res = str.chars().mapToObj(c->(char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(n->n.getValue()==1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+        System.out.println(res);
+    }
+}
