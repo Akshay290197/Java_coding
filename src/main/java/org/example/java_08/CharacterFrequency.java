d9d2cc9c4c7aeee3482c74deb0b41765fc076aaf
Akshay Kumar <cakshay388@gmail.com>
2026-09-05 16:09:39 +0530

    character frequency

--- src/main/java/org/example/java_08/CharacterFrequency.java
+++ src/main/java/org/example/java_08/CharacterFrequency.java
@@ -2,17 +2,18 @@ package org.example.java_08;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CharacterFrequency {
     public static void main(String[] args) {
         String str = "programming";
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        for(Map.Entry<Character, Integer> entry:map.entrySet()){
-            System.out.println(entry.getKey()+":"+entry.getValue());
-        }
+        Map<Character, Long> frequency = str.chars().mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        HashMap::new,
+                        Collectors.counting()
+                ));
+        System.out.println(frequency);
     }
 }
