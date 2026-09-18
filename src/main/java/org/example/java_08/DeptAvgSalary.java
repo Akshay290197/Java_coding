4bbeeb74a0b807ca57218d46ba7c92c84fd158f6
Akshay Kumar <akshay_kumar@cms.co.in>
2026-09-18 15:23:53 +0530

    modification in dept avg salary

--- src/main/java/org/example/java_08/DeptAvgSalary.java
+++ src/main/java/org/example/java_08/DeptAvgSalary.java
@@ -1,5 +1,8 @@
 package org.example.java_08;
 
+import org.example.rev.Employee;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -43,6 +46,21 @@ public class DeptAvgSalary {
         stringIntegerMap.forEach((dept,salary)->{
             System.out.println(dept+":"+salary);
         });
+
+
+        // without java 8
+//        Map<String, Integer> salaries = new HashMap<>();
+//        Map<String, Integer> employeeCount = new HashMap<>();
+//        for(Employee employee:employeeList){
+//            String dept = employee.getDept();
+//            int salary = employee.getSalary();
+//            salaries.put(dept, salaries.getOrDefault(dept,0)+salary);
+//            employeeCount.put(dept, employeeCount.getOrDefault(dept,0)+1);
+//        }
+//        for(String dept : salaries.keySet()){
+//            double averageSalary = (double) salaries.get(dept) / employeeCount.get(dept);
+//            System.out.println(dept+":"+averageSalary);
+//        }
 
     }
 }
