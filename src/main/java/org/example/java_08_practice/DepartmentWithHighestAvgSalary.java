f282b0e6b56e89ac36a54b139a7c747ec07fa3ed
Akshay Kumar <akshay_kumar@cms.co.in>
2026-09-21 15:49:27 +0530

    department with highest average salary

---
+++ src/main/java/org/example/java_08_practice/DepartmentWithHighestAvgSalary.java
@@ -0,0 +1,30 @@
+package org.example.java_08_practice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+/*
+* Find the Department with the Highest Average Salary (Intermediate–Advanced)
+* Use Java 8 Stream API to find the department with the highest average salary.
+* */
+public class DepartmentWithHighestAvgSalary {
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee("A", 50000, "IT"),
+                new Employee("B", 60000, "HR"),
+                new Employee("C", 70000, "IT"),
+                new Employee("D", 80000, "Finance"),
+                new Employee("E", 55000, "HR")
+        );
+
+        employees.stream().collect(Collectors.groupingBy(
+                Employee::getDepartment,
+                Collectors.averagingDouble(Employee::getSalary)
+        )).entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .ifPresent(System.out::println);
+    }
+}
