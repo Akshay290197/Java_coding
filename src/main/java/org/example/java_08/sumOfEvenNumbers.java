3b239bbfb3381405bb29a1080d5356a97017e3fe
Akshay Kumar <akshay_kumar@cms.co.in>
2026-09-21 11:23:59 +0530

    filter even number from list

--- src/main/java/org/example/java_08/sumOfEvenNumbers.java
+++ src/main/java/org/example/java_08/sumOfEvenNumbers.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class sumOfEvenNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30, 35, 40);
-        int sum = numbers.stream().filter(n-> n%2==0)
+        Integer sum = numbers.stream().filter(n-> n%2==0)
                 .reduce((a,b)->a+b)
                 .orElse(null);
         System.out.println(sum);
---
+++ src/main/java/org/example/java_08_practice/FilterEvenNumber.java
@@ -0,0 +1,14 @@
+package org.example.java_08_practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Filter Even Number From List
+* */
+public class FilterEvenNumber {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30, 35, 40);
+        numbers.stream().filter(n->n%2==0).forEach(System.out::println);
+    }
+}
