e1a879a36c75af9f2a3c92de3b6351ec47a95218
Akshay Kumar <akshay_kumar@cms.co.in>
2026-09-24 15:23:42 +0530

    second highest distinct salary

---
+++ src/main/java/org/example/java_08_practice/SecondHighestDistinctSalary.java
@@ -0,0 +1,23 @@
+package org.example.java_08_practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SecondHighestDistinctSalary {
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee("A", 50000, "IT"),
+                new Employee("B", 60000, "HR"),
+                new Employee("C", 70000, "IT"),
+                new Employee("D", 80000, "Finance"),
+                new Employee("E", 55000, "HR"),
+                new Employee("F", 70000, "HR")
+        );
+        employees.stream().map(Employee::getSalary).distinct().sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+    }
+}
