c87196716abcf2e1f6645d4c3e226a7ebc59ee98
Akshay Kumar <akshay_kumar@cms.co.in>
2026-09-24 15:18:31 +0530

    Top 3 highest salaries

---
+++ src/main/java/org/example/java_08_practice/Top3HighestSalary.java
@@ -0,0 +1,20 @@
+package org.example.java_08_practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Top3HighestSalary {
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee("A", 50000, "IT"),
+                new Employee("B", 60000, "HR"),
+                new Employee("C", 70000, "IT"),
+                new Employee("D", 80000, "Finance"),
+                new Employee("E", 55000, "HR")
+        );
+        employees.stream().sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+                .limit(3)
+                .map(Employee::getName).forEach(System.out::println);
+    }
+}
